25db05bed902113a20b21c2c2a1f8dab89ff9ff2
富楠 <funan@zhuojianchina.com>
2020-12-29 18:00:20 +0800

    12.29

--- src/components/shop/foodList.vue
+++ src/components/shop/foodList.vue
@@ -3,20 +3,35 @@
     <van-sticky>
       a
     </van-sticky>
-    <div>
-      b
+    <div class="shop-food-item" v-for="(item, index) in list" :key="index">
+      {{item.name}}
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-  
+  setup() {
+    const list = ref([{
+      name: 'zzzzzzzzz'
+    },{
+      name: 'zxzxxxxxx'
+    }])
+    return { list }
+  }
 }
 </script>
 
 <style lang="scss">
 .shop-food-list {
   overflow-y: auto;
+  width: calc(100% - 80px);
+  .shop-food-item {
+    padding: 5px;
+    margin: 5px;
+    border-radius: 5px;
+    background: #f7f7f7;
+  }
 }
 </style>
